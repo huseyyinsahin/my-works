dd22c453d49bd92824b1275b8a31fa879a75d745
huseyin <93516358+huseyyinsahin@users.noreply.github.com>
2023-10-16 12:24:12 +0300

    JavaScript update.

---
+++ 3-javascript/Yeni Kurs/Bölüm 1/1-Alert/can.js
@@ -0,0 +1,2 @@
+ alert('merhaba dünya');
+   
---
+++ 3-javascript/Yeni Kurs/Bölüm 1/3-DeğiskenTanımlama/can.js
@@ -0,0 +1,23 @@
+let gun=14;
+let ay=7;
+let yil=1992;
+
+console.log(gun,ay,yil);
+
+
+console.log("Can Boz " +gun+"/"+ay+"/"+yil+" tarihinde doğmuştur.");
+
+gun=29;
+console.log(gun);
+
+var arabafiyati=130000;
+console.log(arabafiyati);
+
+const sifirarabafiyatı =250000;
+console.log(sifirarabafiyatı);
+
+//a
+
+/*
+a
+*/
