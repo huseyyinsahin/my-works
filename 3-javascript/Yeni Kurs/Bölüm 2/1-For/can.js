d5d033b742367617ab4f7985f9b081f6bf402e88
huseyin <93516358+huseyyinsahin@users.noreply.github.com>
2023-10-18 23:48:01 +0300

    Update javascript

---
+++ 3-javascript/Yeni Kurs/Bölüm 2/1-For/can.js
@@ -0,0 +1,14 @@
+for(let i=0;i<10;i++)
+{
+    console.log(i);
+}
+console.log("Döngü Bitti");
+
+
+const adSoyad=['can','tuçe','elif'];
+for(let i=0;adSoyad.length;i++)
+{
+    console.log(adSoyad[i])
+    let html=`<div>${adSoyad[İ]}</div>`;
+    console.log(html);
+}
---
+++ 3-javascript/Yeni Kurs/Bölüm 2/2-While/can.js
@@ -0,0 +1,20 @@
+
+const adSoyad=['can','tuçe','elif'];
+
+let i=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
+i=0;
+while(i<adSoyad.length){
+    console.log(adSoyad[i]);
+    i++;
+}
+
+i=6;
+while(i>3)
+{
+    console.log(i);
+    i--;
+}
