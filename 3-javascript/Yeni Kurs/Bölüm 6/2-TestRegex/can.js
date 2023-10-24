8c50a1dc85c8c72e0898194bd6a0863e3daa6e5d
huseyin <93516358+huseyyinsahin@users.noreply.github.com>
2023-10-24 20:17:25 +0300

    Javascript update

---
+++ 3-javascript/Yeni Kurs/Bölüm 6/2-TestRegex/can.js
@@ -0,0 +1,21 @@
+const username='111canboz';
+const pattern=/^[a-z]{6,10}$/;
+
+
+// let sonuc=pattern.test(username);
+// console.log(sonuc);
+
+
+// if(sonuc)
+// {
+// console.log('Başarılı');
+// }
+// else
+// {
+// console.log('başarısız');
+
+// }
+
+
+let sonuc=username.search(pattern);
+console.log(sonuc);
