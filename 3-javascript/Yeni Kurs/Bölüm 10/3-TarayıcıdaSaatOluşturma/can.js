4b026f4ae65e46fc68a116e6c5695e108a86514d
huseyin <93516358+huseyyinsahin@users.noreply.github.com>
2023-10-30 13:13:19 +0300

    Javascript update

--- 3-javascript/Yeni Kurs/Bölüm 10/3-TarayıcıdaSaatOluşturma/can.js
+++ 3-javascript/Yeni Kurs/Bölüm 10/3-TarayıcıdaSaatOluşturma/can.js
@@ -1,6 +1,7 @@
 const clock =document.querySelector('.clock');
 
 
+
 const tick=()=>{
 const now=new Date();
 const hours=now.getHours();
--- 3-javascript/Yeni Kurs/Bölüm 11/2-TodoEkleme/can.js
+++ 3-javascript/Yeni Kurs/Bölüm 11/2-TodoEkleme/can.js
@@ -3,6 +3,7 @@ const list=document.querySelector('.todos');
 
 
 
+
 const generateTemplate = todo=>{
 const html=`
 <li class="list-group-item d-flex justify-content-between align-items-center">
