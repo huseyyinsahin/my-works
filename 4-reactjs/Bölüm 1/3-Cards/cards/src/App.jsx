6958444e9e224df96cb1ab8966a45c6ad90b6732
huseyyinsahin <huseysahin19@outlook.com>
2024-01-10 22:24:54 +0300

    React JS update

--- 4-reactjs/Bölüm 1/3-Cards/cards/src/App.jsx
+++ 4-reactjs/Bölüm 1/3-Cards/cards/src/App.jsx
@@ -1,19 +1,34 @@
 import "./App.css";
 import Course from "./Course";
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/bootstrap5.png";
+import Csharp from "./images/ccsharp.png";
+import KompleWeb from "./images/kompleweb.jpg";
 
 function App() {
   return (
     <div className="App">
+      {/* <image src={Angular} alt="" /> */}
       <Course
+        image={Angular}
         title="Angular"
         description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique dolor tenetur, ullam non velit aliquam excepturi accusantium provident eos, repellat atque consectetur, exercitationem voluptatem quas aperiam? Praesentium, magnam dolorem! Quo."
       />
-      <Course title="Bootstrap 5"
-        description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique dolor tenetur, ullam non velit aliquam excepturi accusantium provident eos, repellat atque consectetur, exercitationem voluptatem quas aperiam? Praesentium, magnam dolorem! Quo." />
-      <Course title="Komple Web"
-        description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique dolor tenetur, ullam non velit aliquam excepturi accusantium provident eos, repellat atque consectetur, exercitationem voluptatem quas aperiam? Praesentium, magnam dolorem! Quo." />
-      <Course title="Front end"
-        description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique dolor tenetur, ullam non velit aliquam excepturi accusantium provident eos, repellat atque consectetur, exercitationem voluptatem quas aperiam? Praesentium, magnam dolorem! Quo." />
+      <Course
+        image={Bootstrap}
+        title="Bootstrap 5"
+        description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique dolor tenetur, ullam non velit aliquam excepturi accusantium provident eos, repellat atque consectetur, exercitationem voluptatem quas aperiam? Praesentium, magnam dolorem! Quo."
+      />
+      <Course
+        image={Csharp}
+        title="Komple Web"
+        description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique dolor tenetur, ullam non velit aliquam excepturi accusantium provident eos, repellat atque consectetur, exercitationem voluptatem quas aperiam? Praesentium, magnam dolorem! Quo."
+      />
+      <Course
+        image={KompleWeb}
+        title="Front end"
+        description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique dolor tenetur, ullam non velit aliquam excepturi accusantium provident eos, repellat atque consectetur, exercitationem voluptatem quas aperiam? Praesentium, magnam dolorem! Quo."
+      />
     </div>
   );
 }
