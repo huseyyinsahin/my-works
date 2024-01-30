0fd44bc091a6d24785c999a7f8fad0c86c87c357
huseyyinsahin <huseysahin19@outlook.com>
2024-01-30 17:03:19 +0300

    React JS update

---
+++ 4-reactjs/React JS Dersleri ve Projeleri/10-Kurslarim Projesi/kurslarim/src/Courses.jsx
@@ -0,0 +1,25 @@
+import Course from "./Course";
+
+function Courses({ courses, removeCourse }) {
+  return (
+    <div className="courseMainDiv">
+      <div className="">
+        <h2>Kurslarım</h2>
+      </div>
+      <div className="cardDiv">
+        {courses.map((course) => {
+          return (
+            <Course
+              key={course.id}
+              {...course}
+              removeOneCourse={removeCourse}
+            />
+            //course={course}
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Courses;
