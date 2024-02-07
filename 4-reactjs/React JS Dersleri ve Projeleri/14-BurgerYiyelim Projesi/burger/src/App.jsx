e973f6446c11e003e90c53b04182a34c298674ee
huseyyinsahin <huseysahin19@outlook.com>
2024-02-07 17:24:32 +0300

    React JS update

--- 4-reactjs/React JS Dersleri ve Projeleri/14-BurgerYiyelim Projesi/burger/src/App.jsx
+++ 4-reactjs/React JS Dersleri ve Projeleri/14-BurgerYiyelim Projesi/burger/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Menu from "./components/Menu";
+import { Menu } from "./components/Menu";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import { Navbar } from "./components/Navbar";
--- 4-reactjs/React JS Dersleri ve Projeleri/14-BurgerYiyelim Projesi/burger/src/components/Home.jsx
+++ 4-reactjs/React JS Dersleri ve Projeleri/14-BurgerYiyelim Projesi/burger/src/components/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/banneryeni.jpg";
+import "../style/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div
+      className="mainPage"
+      style={{ backgroundImage: `url(${BannerImage})` }}
+    >
+      <div className="order">
+        <Link to="/menu">
+          <button>SİPARİŞ VER</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
