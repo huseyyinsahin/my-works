8e12e576734bd88653864090216816843f191262
huseyyinsahin <huseysahin19@outlook.com>
2024-02-24 16:30:35 +0300

    React JS update

---
+++ 4-reactjs/React JS Dersleri ve Projeleri/16-Typescript/1-Kurulum/app.js
@@ -0,0 +1,32 @@
+// 1.
+// console.log("Typescript e girdikssss")
+// console.log("hüseyin")
+// console.log("sahin")
+// 2.
+// let age: number = 29;
+// let firstname: string = "Hüseyin";
+// let lastname: string = "sahin";
+// let isUpdated: boolean = true;
+// function display(id: any, name: string) {
+//   console.log("Id=" + id + ", Name" + name);
+// }
+// 3.
+// let first: number = 123;
+// let second: number = 0x37cf;
+// let third: number = 0o377;
+// let fourth: number = 0b111001;
+// console.log(first);
+// console.log(second);
+// console.log(third);
+// console.log(fourth);
+// let employeefirstName: string = "Can Boz";
+// console.log(employeefirstName);
+// let employeeName: string = "Jhon smith";
+// let employeeDept: string = "Finance";
+// let employeeDesc1: string =
+//   employeeName + " works in the " + employeeDept + " department.";
+// let employeeDesc2: string = `${employeeName} works in the ${employeeDept} department.`;
+// console.log(employeeDesc1);
+// console.log(employeeDesc2);
+// let isPresent:boolean=true;
+// console.log(isPresent);
