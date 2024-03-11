819ad4764de0ca9047ffdf7541c87b3fd6b1378b
huseyyinsahin <huseysahin19@outlook.com>
2024-03-11 14:09:54 +0300

    React JS update

--- 4-reactjs/React JS Dersleri ve Projeleri/16-Typescript/6-Type Guard/app.js
+++ 4-reactjs/React JS Dersleri ve Projeleri/16-Typescript/6-Type Guard/app.js
@@ -38,27 +38,110 @@
 //   return message;
 // }
 // 2. Generics
-function getRandomNumber(items) {
-    var randomIndex = Math.floor(Math.random() * items.length);
-    return items[randomIndex];
-}
-var numbers = [1, 54, 65, 7, 8];
-// console.log(getRandomNumber(numbers));
-function getRandomString(items) {
-    var randomIndex = Math.floor(Math.random() * items.length);
-    return items[randomIndex];
-}
-var adlarim = ["Can", "Tuba", "Merve"];
-// console.log(getRandomString(adlarim));
-// function getRandomElement(items: any[]): any {
-//     let randomIndex = Math.floor(Math.random() * items.length);
-//     return items[randomIndex];
+// function getRandomNumber(items: number[]): number {
+//   let randomIndex = Math.floor(Math.random() * items.length);
+//   return items[randomIndex];
 // }
-var degiskenlerim = [true, false, true];
-function getRandomElement(items) {
-    var randomIndex = Math.floor(Math.random() * items.length);
-    return items[randomIndex];
+// let numbers = [1, 54, 65, 7, 8];
+// // console.log(getRandomNumber(numbers));
+// function getRandomString(items: string[]): string {
+//   let randomIndex = Math.floor(Math.random() * items.length);
+//   return items[randomIndex];
+// }
+// let adlarim = ["Can", "Tuba", "Merve"];
+// // console.log(getRandomString(adlarim));
+// // function getRandomElement(items: any[]): any {
+// //     let randomIndex = Math.floor(Math.random() * items.length);
+// //     return items[randomIndex];
+// // }
+// let degiskenlerim = [true, false, true];
+// function getRandomElement<T>(items: T[]): T {
+//   let randomIndex = Math.floor(Math.random() * items.length);
+//   return items[randomIndex];
+// }
+// console.log(getRandomElement<number>(numbers));
+// console.log(getRandomElement<string>(adlarim));
+// console.log(getRandomElement(degiskenlerim));
+// 3.Generic Constrains
+// function merge<U extends object, V extends object>(obj1: U, obj2: V) {
+//   return {
+//     ...obj1,
+//     ...obj2,
+//   };
+// }
+// let person = merge(
+//   { name: "Can" },
+//   // { age: 29 }
+//   29
+// );
+// console.log(person);
+// 4. Interface Generic
+// interface Months<U, V> {
+//     key: U,
+//     value: V
+// }
+// let month: Months<number, string> = {
+//     key: 1,
+//     value: 'January'
+// }
+// console.log(month);
+// interface Collection<T> {
+//   add(o: T): void;
+//   remove(o: T): void;
+// }
+// class List<T> implements Collection<T> {
+//   private items: T[] = [];
+//   add(o: T): void {
+//     this.items.push(o);
+//     console.log(this.items);
+//   }
+//   remove(o: T): void {
+//     let index = this.items.indexOf(o);
+//     if (index > -1) {
+//       this.items.splice(index, 1);
+//     }
+//   }
+// }
+// let list = new List<number>();
+// for (let i = 0; i < 10; i++) {
+//   list.add(i);
+// }
+// 5. Class Generic
+var Stack = /** @class */ (function () {
+    function Stack(size) {
+        this.size = size;
+        this.elements = [];
+    }
+    Stack.prototype.isEmpty = function () {
+        return this.elements.length === 0;
+    };
+    Stack.prototype.isFull = function () {
+        return this.elements.length === this.size;
+    };
+    Stack.prototype.push = function (element) {
+        if (this.elements.length === this.size) {
+            throw new Error("The stack is overflow!");
+        }
+        this.elements.push(element);
+    };
+    Stack.prototype.pop = function () {
+        if (this.elements.length == 0) {
+            throw new Error("The stack is empty!");
+        }
+        return this.elements.pop();
+    };
+    return Stack;
+}());
+function randBetween(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
 }
-console.log(getRandomElement(numbers));
-console.log(getRandomElement(adlarim));
-console.log(getRandomElement(degiskenlerim));
+var numbers = new Stack(5);
+while (!numbers.isFull()) {
+    var n = randBetween(1, 10);
+    console.log("Push ".concat(n, " into the stack."));
+    numbers.push(n);
+}
+while (!numbers.isEmpty()) {
+    var n = numbers.pop();
+    console.log("Pop ".concat(n, " from the stack."));
+}
--- 4-reactjs/React JS Dersleri ve Projeleri/16-Typescript/6-Type Guard/app.ts
+++ 4-reactjs/React JS Dersleri ve Projeleri/16-Typescript/6-Type Guard/app.ts
@@ -51,32 +51,132 @@
 
 // 2. Generics
 
-function getRandomNumber(items: number[]): number {
-  let randomIndex = Math.floor(Math.random() * items.length);
-  return items[randomIndex];
-}
-
-let numbers = [1, 54, 65, 7, 8];
-// console.log(getRandomNumber(numbers));
-
-function getRandomString(items: string[]): string {
-  let randomIndex = Math.floor(Math.random() * items.length);
-  return items[randomIndex];
-}
-
-let adlarim = ["Can", "Tuba", "Merve"];
-// console.log(getRandomString(adlarim));
-
-// function getRandomElement(items: any[]): any {
-//     let randomIndex = Math.floor(Math.random() * items.length);
-//     return items[randomIndex];
+// function getRandomNumber(items: number[]): number {
+//   let randomIndex = Math.floor(Math.random() * items.length);
+//   return items[randomIndex];
 // }
 
-let degiskenlerim = [true, false, true];
-function getRandomElement<T>(items: T[]): T {
-  let randomIndex = Math.floor(Math.random() * items.length);
-  return items[randomIndex];
+// let numbers = [1, 54, 65, 7, 8];
+// // console.log(getRandomNumber(numbers));
+
+// function getRandomString(items: string[]): string {
+//   let randomIndex = Math.floor(Math.random() * items.length);
+//   return items[randomIndex];
+// }
+
+// let adlarim = ["Can", "Tuba", "Merve"];
+// // console.log(getRandomString(adlarim));
+
+// // function getRandomElement(items: any[]): any {
+// //     let randomIndex = Math.floor(Math.random() * items.length);
+// //     return items[randomIndex];
+// // }
+
+// let degiskenlerim = [true, false, true];
+// function getRandomElement<T>(items: T[]): T {
+//   let randomIndex = Math.floor(Math.random() * items.length);
+//   return items[randomIndex];
+// }
+// console.log(getRandomElement<number>(numbers));
+// console.log(getRandomElement<string>(adlarim));
+// console.log(getRandomElement(degiskenlerim));
+
+// 3.Generic Constrains
+
+// function merge<U extends object, V extends object>(obj1: U, obj2: V) {
+//   return {
+//     ...obj1,
+//     ...obj2,
+//   };
+// }
+
+// let person = merge(
+//   { name: "Can" },
+//   // { age: 29 }
+//   29
+// );
+
+// console.log(person);
+
+// 4. Interface Generic
+
+// interface Months<U, V> {
+//     key: U,
+//     value: V
+// }
+
+// let month: Months<number, string> = {
+//     key: 1,
+//     value: 'January'
+// }
+
+// console.log(month);
+
+// interface Collection<T> {
+//   add(o: T): void;
+//   remove(o: T): void;
+// }
+
+// class List<T> implements Collection<T> {
+//   private items: T[] = [];
+//   add(o: T): void {
+//     this.items.push(o);
+//     console.log(this.items);
+//   }
+
+//   remove(o: T): void {
+//     let index = this.items.indexOf(o);
+//     if (index > -1) {
+//       this.items.splice(index, 1);
+//     }
+//   }
+// }
+
+// let list = new List<number>();
+
+// for (let i = 0; i < 10; i++) {
+//   list.add(i);
+// }
+
+
+// 5. Class Generic
+
+class Stack<T> {
+  private elements: T[] = [];
+
+  constructor(private size: number) {}
+  isEmpty(): boolean {
+    return this.elements.length === 0;
+  }
+  isFull(): boolean {
+    return this.elements.length === this.size;
+  }
+  push(element: T): void {
+    if (this.elements.length === this.size) {
+      throw new Error("The stack is overflow!");
+    }
+    this.elements.push(element);
+  }
+  pop(): T {
+    if (this.elements.length == 0) {
+      throw new Error("The stack is empty!");
+    }
+    return this.elements.pop();
+  }
 }
-console.log(getRandomElement<number>(numbers));
-console.log(getRandomElement<string>(adlarim));
-console.log(getRandomElement(degiskenlerim));
+
+function randBetween(low: number, high: number): number {
+  return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+let numbers = new Stack<number>(5);
+
+while (!numbers.isFull()) {
+  let n = randBetween(1, 10);
+  console.log(`Push ${n} into the stack.`);
+  numbers.push(n);
+}
+while (!numbers.isEmpty()) {
+  let n = numbers.pop();
+  console.log(`Pop ${n} from the stack.`);
+}
